329e10c464f614ceb91428a5a4fe03ccf55b99d5
Tracy-115 <kmin984@insite.4cd.edu>
2025-12-10 18:25:51 -0800

    working on milestone 1

--- final.cpp
+++ final.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <map>
+
+using namespace std;
+int main(){
+  map<string, int>;
+  ifstream infile("210-final-1-FA25.txt");
+  if(!infile){
+    cout << "Error in opening file" << endl;
+  }
+  string loca, desti;
+  
+  
+}
 
