d86f352e2ef61f1565c475eb1e4e5521e1ae466a
Tracy-115 <kmin984@insite.4cd.edu>
2025-12-10 18:31:48 -0800

    working on milestone 1

--- final.cpp
+++ final.cpp
@@ -6,13 +6,28 @@
 
 using namespace std;
 int main(){
-  map<string, int>;
+  map<string, int> traff;
   ifstream infile("210-final-1-FA25.txt");
   if(!infile){
     cout << "Error in opening file" << endl;
   }
   string loca, desti;
-  
+  while (infile >> loca >> desti) {
+
+        if (traff.find(loca) = traffic.end()) {
+            traff[loca] = 0;
+        }
+        traff[loca]++;
+
+        if (trafffind(desti) = traffc.end()) {
+            traff[desti] = 0;
+        }
+        traff[desti]++;
+    }
+  cout << "All airport traffic counts:" << endl;
+  for (auto &p : traff) {
+      cout << p.first << " " << p.second << endl;
+  }
   
 }
 
